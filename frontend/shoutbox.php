d5bcbb200d3b37f743c9460261bd6175a1576e48
rkuipers <rkuipers@26720b1a-1515-4208-a898-b861cbbdf37f>
2006-04-08 19:59:24 +0000

    Added regex to change URL's into actual links
    git-svn-id: http://www.rkpk.nl/svn/dpcstats@105 26720b1a-1515-4208-a898-b861cbbdf37f

--- frontend/shoutbox.php
+++ frontend/shoutbox.php
@@ -17,7 +17,13 @@ for($i=0;$i<count($messages);$i++)
 	echo ' - ' . $messages[$i]->getTijd() . '</td>';
 	echo '</tr>';
 	echo trBackground( ( $i + 1 ) );
-	echo '<td>' . $messages[$i]->getBericht() . '</td>';
+	echo '<td>';
+	
+	# Replace complete URL's with an actual link
+	echo preg_replace(	array('/http\:\/\/(.\*)(\b|$|\s|\n||\)|\z|\Z)/'),
+				array('<a href="\0">link</a>'),
+				$messages[$i]->getBericht());
+	echo '</td>';
 	echo '</tr>';
 	echo '<tr><td><br></td></tr>';
 }
