8b7a1c56a07e8b62e8a54e42339788b295265673
rkuipers <rkuipers@26720b1a-1515-4208-a898-b861cbbdf37f>
2006-04-24 08:04:27 +0000

    Backend class file for the new faq
    git-svn-id: http://www.rkpk.nl/svn/dpcstats@129 26720b1a-1515-4208-a898-b861cbbdf37f

---
+++ frontend/classes/faq.php
@@ -0,0 +1,41 @@
+<?
+
+class faq
+{
+	private $entries;
+
+	private $db;
+	
+	public function __construct($db)
+	{
+		$this->db = $db;
+
+		$entries = array();
+
+		$this->gatherQuestions();
+	}
+
+	private function gatherQuestions()
+	{
+		$query = 'SELECT
+				question,
+				answer
+			FROM
+				faq';
+
+		$result = $this->db->selectQuery($query);
+
+		while ( $line = $this->db->fetchArray($result) )
+		{
+			$this->entries[] = array('question' => $line['question'],
+						'answer' => $line['answer']);
+		}
+	}
+
+	public function getEntries()
+	{
+		return $this->entries;
+	}
+}
+
+?>
