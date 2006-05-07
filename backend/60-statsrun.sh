1c2a71dca35bbe38ea129c89ec210172b6d8a784
rkuipers <rkuipers@26720b1a-1515-4208-a898-b861cbbdf37f>
2006-05-07 18:34:01 +0000

    Removed commented lines to run f@h stats, stampede stats and moved average script to a seperate crontab entry.
    git-svn-id: http://www.rkpk.nl/svn/dpcstats@155 26720b1a-1515-4208-a898-b861cbbdf37f

--- backend/60-statsrun.sh
+++ backend/60-statsrun.sh
@@ -6,13 +6,6 @@
 
 /home/rkuipers/stats/statsrun/smp.php
 
-#/home/rkuipers/stats/statsrun/fah-teams.php
-#/home/rkuipers/stats/statsrun/fah-members.php
-
 /home/rkuipers/stats/statsrun/ufl.php
 
 /home/rkuipers/stats/statsrun/individual.php
-
-/home/rkuipers/stats/statsrun/stampede.php
-
-/home/rkuipers/stats/statsrun/average.php
