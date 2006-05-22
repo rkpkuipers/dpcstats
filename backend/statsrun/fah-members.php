13dd38aa23d961fbe090910e364839ea9894dcf2
rkuipers <rkuipers@26720b1a-1515-4208-a898-b861cbbdf37f>
2006-05-22 12:18:39 +0000

    Moved code to retrieve data from stanford to a function and added a do..while loop to continue calling the function until data is returned.
    git-svn-id: http://www.rkpk.nl/svn/dpcstats@172 26720b1a-1515-4208-a898-b861cbbdf37f

--- backend/statsrun/fah-members.php
+++ backend/statsrun/fah-members.php
@@ -38,120 +38,141 @@ dailyOffset('memberoffset', 'fah');
 dailyOffset('subteamoffset', 'fah');
 dailyOffset('individualoffset', 'fah');
 
-$tempDir = '/home/rkuipers/stats/statsrun/files/';
-system('wget "http://fah-web.stanford.edu/cgi-bin/main.py?qtype=teampage&teamnum=92" -q -O ' . $tempDir . '/fah-members.html');
-system('cat ' . $tempDir . '/fah-members.html | grep -e fah-web.stanford.edu\/awards\/cert.php -e qtype=userpage | grep -v t=wus > ' . $tempDir . '/fah-members.txt');
-unlink($tempDir . '/fah-members.html');
-
-$raw = implode('', file($tempDir . '/fah-members.txt'));
-$data = preg_replace(array('@<a[^>]*?>@si', '@</a>@si', '@<td[^>]*?>@si', '@</td>@si', '@<TD[^>]*?>@si', '@</TD>@si'), '||', $raw);
-
-unlink($tempDir . '/fah-members.txt');
-
-$info = explode('||', $data);
-
 $members = array();
-$subteams = array();
 $subteamMembers = array();
 
-for($i=2;$i<count($info);$i+=8)
+function getMembers()
 {
-	#echo 'user: ' . trim($info[$i]) . ' score: ' . trim($info[$i+4]) . "\n";
-
-	$user = trim($info[$i]);
-	$score = trim($info[$i+4]);
-
-	if ( $score == 0 ) break;
+	global $datum, $members, $subteamMembers;
 	
-	$team = getSubteam($user);
-	if ( $team != '' )
+	$tempDir = '/home/rkuipers/stats/statsrun/files/';
+	system('wget "http://fah-web.stanford.edu/cgi-bin/main.py?qtype=teampage&teamnum=92" -q -O ' . $tempDir . '/fah-members.html');
+	system('cat ' . $tempDir . '/fah-members.html | grep -e fah-web.stanford.edu\/awards\/cert.php -e qtype=userpage | grep -v t=wus > ' . $tempDir . '/fah-members.txt');
+	unlink($tempDir . '/fah-members.html');
+
+	$raw = implode('', file($tempDir . '/fah-members.txt'));
+	$data = preg_replace(array('@<a[^>]*?>@si', '@</a>@si', '@<td[^>]*?>@si', '@</td>@si', '@<TD[^>]*?>@si', '@</TD>@si'), '||', $raw);
+
+	unlink($tempDir . '/fah-members.txt');
+
+	$info = explode('||', $data);
+
+	$members = array();
+	$subteams = array();
+	$subteamMembers = array();
+
+	for($i=2;$i<count($info);$i+=8)
 	{
-		$subteams[$team] += $score;
-		$subteamMembers[$team][$user] = $score;
-	}
-	elseif ( is_numeric(strpos($user, '0')) )
-	{
-		$pos = strpos($user, '0');
-		$team = substr($user, 0, $pos);
-		$user = substr($user, ( $pos + 1 ) );
-		#echo $user . ' - ' . $team . "\n";
+		#echo 'user: ' . trim($info[$i]) . ' score: ' . trim($info[$i+4]) . "\n";
+
+		$user = trim($info[$i]);
+		$score = trim($info[$i+4]);
+
+		if ( $score == 0 ) break;
+	
+		$team = getSubteam($user);
+		if ( $team != '' )
+		{
+			$subteams[$team] += $score;
+			$subteamMembers[$team][$user] = $score;
+		}
+		elseif ( is_numeric(strpos($user, '0')) )
+		{
+			$pos = strpos($user, '0');
+			$team = substr($user, 0, $pos);
+			$user = substr($user, ( $pos + 1 ) );
+			#echo $user . ' - ' . $team . "\n";
 		
-		$set = 0;
+			$set = 0;
+			foreach($subteams as $stName => $stScore)
+			{
+				if ( strtoupper($stName) == strtoupper($team) )
+				{
+					$set = 1;
+					$subteams[$stName] += $score;
+				
+					if ( ! isset($subteamMembers[$stName][$user]) )
+						$subteamMembers[$stName][$user] = $score;
+					else
+						$subteamMembers[$stName][$user] += $score;
+				}
+			}
+
+			if ( $set == 0 )
+			{
+				$subteams[$team] = $score;
+				
+				$subteamMembers[$team] = array();
+
+				if ( ! isset($subteamMembers[$team][$user]) )
+					$subteamMembers[$team][$user] = $score;
+				else
+					$subteamMembers[$team][$user] += $score;
+			}
+		}
+		else
+		{
+			$set = 0;
+			foreach($members as $mName => $mScore)
+			{
+				if ( strtoupper($mName) == strtoupper($user) )
+				{
+					$members[$mName] += $score;
+					$set++;
+				}
+			}
+		
+			if ( $set == 0 )
+				$members[$user] = $score;
+		}
+	}
+
+	foreach($members as $mName => $mScore)
+	{
 		foreach($subteams as $stName => $stScore)
 		{
-			if ( strtoupper($stName) == strtoupper($team) )
+			if ( strtoupper($mName) == strtoupper($stName) )
 			{
-				$set = 1;
-				$subteams[$stName] += $score;
-				
-				if ( ! isset($subteamMembers[$stName][$user]) )
-					$subteamMembers[$stName][$user] = $score;
+				#$stScore += $mScore;
+				$subteams[$stName] += $mScore;
+				if ( ! isset($subteamMembers[$stName]) )
+					$subteamMembers[$stName] = $mScore;
 				else
-					$subteamMembers[$stName][$user] += $score;
+					$subteamMembers[$stName][$mName] += $mScore;
+				#echo $mName . "\n";
 			}
 		}
-
-		if ( $set == 0 )
-		{
-			$subteams[$team] = $score;
-				
-			$subteamMembers[$team] = array();
-
-			if ( ! isset($subteamMembers[$team][$user]) )
-				$subteamMembers[$team][$user] = $score;
-			else
-				$subteamMembers[$team][$user] += $score;
-		}
 	}
-	else
+
+	foreach ( $subteams as $name => $score )
 	{
-		$set = 0;
-		foreach($members as $mName => $mScore)
+		if ( count($subteamMembers[$name] ) > 1 )
 		{
-			if ( strtoupper($mName) == strtoupper($user) )
-			{
-				$members[$mName] += $score;
-				$set++;
-			}
+			#$members[$name] = $score;
+			#echo $name . ' ' . $score . "\n";
+			if ( $name != '' )
+				$members[$name] = $score;
 		}
-		
-		if ( $set == 0 )
-			$members[$user] = $score;
+		else
+		{
+			foreach($subteamMembers[$name] as $temp =>  $tmpname)
+				$members[$name . '0' . $temp] = $score;
+		}
 	}
 }
 
-foreach($members as $mName => $mScore)
+$itt = 0;
+do
 {
-	foreach($subteams as $stName => $stScore)
+	if ( $itt > 0 )
 	{
-		if ( strtoupper($mName) == strtoupper($stName) )
-		{
-			#$stScore += $mScore;
-			$subteams[$stName] += $mScore;
-			if ( ! isset($subteamMembers[$stName]) )
-				$subteamMembers[$stName] = $mScore;
-			else
-				$subteamMembers[$stName][$mName] += $mScore;
-			#echo $mName . "\n";
-		}
+		echo "No members retrieved, restarting loop\n";
+		sleep(10);
 	}
-}
-
-foreach ( $subteams as $name => $score )
-{
-	if ( count($subteamMembers[$name] ) > 1 )
-	{
-		#$members[$name] = $score;
-		#echo $name . ' ' . $score . "\n";
-		if ( $name != '' )
-			$members[$name] = $score;
-	}
-	else
-	{
-		foreach($subteamMembers[$name] as $temp =>  $tmpname)
-			$members[$name . '0' . $temp] = $score;
-	}
-}
+	$itt++;
+	
+	getMembers();
+} while ( ( count($members) == 0 ) || ( count($subteamMembers) == 0 ) );
 
 arsort($members, SORT_NUMERIC);
 foreach($members as $name => $score)
