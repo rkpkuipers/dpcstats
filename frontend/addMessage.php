681ab611e39eb5a928fb8fc338b668ead6a86289
rkuipers <rkuipers@26720b1a-1515-4208-a898-b861cbbdf37f>
2006-05-23 18:51:03 +0000

    Restored proper files.
    git-svn-id: http://www.rkpk.nl/svn/dpcstats@188 26720b1a-1515-4208-a898-b861cbbdf37f

---
+++ frontend/addMessage.php
@@ -0,0 +1,85 @@
+<?
+
+include ('classes.php');
+
+function parseCode($text)
+{
+// Array of tags with opening and closing
+	$tagArray['img'] = array('open'=>'<img src="','close'=>'">');
+	$tagArray['b'] = array('open'=>'<b>','close'=>'</b>');
+	$tagArray['i'] = array('open'=>'<i>','close'=>'</i>');
+	$tagArray['u'] = array('open'=>'<u>','close'=>'</u>');
+	$tagarray['br'] = array('open'=>'<br>','close'=>'');
+	#$tagArray['url'] = array('open'=>'<a href="','close'=>'">\\1</a>');
+	#$tagArray['email'] = array('open'=>'<a href="mailto:','close'=>'">\\1</a>');
+	$tagArray['url=(.*)'] = array('open'=>'<a href="','close'=>'">\\2</a>');
+	$tagArray['email=(.*)'] = array('open'=>'<a href="mailto:','close'=>'">\\2</a>');
+	$tagArray['color=(.*)'] = array('open'=>'<font color="','close'=>'">\\2</font>');
+	$tagArray['size=(.*)'] = array('open'=>'<font size="','close'=>'">\\2</font>');
+	$tagArray['font=(.*)'] = array('open'=>'<font face="','close'=>'">\\2</font>');
+
+// Array of tags with only one part
+	$sTagArray['br'] = array('tag'=>'<br>');
+	$sTagArray['hr'] = array('tag'=>'<hr>');
+
+	foreach($tagArray as $tagName=>$replace)
+	{
+		$tagEnd=preg_replace('/\W/Ui','',$tagName);
+		$text = preg_replace("|\[$tagName\](.*)\[/$tagEnd\]|Ui","$replace[open]\\1$replace[close]",$text);
+	}
+
+	foreach($sTagArray as $tagName=>$replace)
+	{
+		$text= preg_replace("|\[$tagName\]|Ui","$replace[tag]",$text);
+	}
+
+return $text;
+}
+
+if ( isset($HTTP_GET_VARS['bericht']) )
+	$bericht = $HTTP_GET_VARS['bericht'];
+else
+	$bericht = '';
+
+if ( isset($HTTP_GET_VARS['poster']) )
+	$poster = $HTTP_GET_VARS['poster'];
+else
+	$poster = '';
+
+if ( isset($HTTP_GET_VARS['email']) )
+	$email = $HTTP_GET_VARS['email'];
+else
+	$email = '';
+
+if ( isset($_GET['tabel']) )
+	$tabel = $_GET['tabel'];
+
+if ( isset($_GET['prefix']) )
+	$prefix = $_GET['prefix'];
+
+if ( isset($_REQUEST['team']) )
+	$team = $_REQUEST['team'];
+
+if ( ( $poster != '' ) && ( $bericht != '' ) )
+{
+	$query = 'INSERT INTO 
+			shoutbox 
+		VALUES 
+		(
+			\'' . $poster . '\', 
+			\'' . parseCode(htmlspecialchars($bericht)) . '\', 
+			\''. date("Y-m-d H:i:s") . '\',
+			\'' . $email . '\')';
+ 	$db->selectQuery($query);
+
+	$recipient = 'Remko Kuipers <rkpkuipers@planet.nl>';
+	$subject = 'Shoutbox post by ' . $poster;
+	$message = $poster . ' (' . $email . ') posted a message on ' . date("Y-m-d H:i:s") .
+			"\n\n" . parseCode(htmlspecialchars($bericht));
+	
+	mail($recipient, $subject, $message);
+ }
+
+ // page ending rejumping 2 index.
+ header("Location: index.php?prefix=" . $prefix . '&tabel=' . $tabel . '&team=' . rawurlencode($team));
+ ?>
---
+++ frontend/average.php
@@ -0,0 +1,68 @@
+<br>
+<center>
+<table width="100%">
+<tr>
+ <td width="25%" align="center">
+  <form action="index.php">
+  <input type="hidden" name="mode" value="avgProd">
+  <input type="hidden" name="tabel" value="memberoffset">
+  <input type="hidden" name="prefix" value="<? echo $project->getPrefix(); ?>">
+  <input type="Submit" value="DPC Members" class="TextField">
+  </form>
+ </td>
+ <td width="25%" align="center">
+   <form action="index.php">
+   <input type="hidden" name="mode" value="avgProd">
+   <input type="hidden" name="tabel" value="teamoffset">
+   <input type="Submit" value="Teams" class="TextField">
+   <input type="hidden" name="prefix" value="<? echo $project->getPrefix(); ?>">
+   </form>
+ </td>
+<?
+if ( in_array($project->getPrefix(), array('fah', 'rah', 'sah', 'smp', 'sob', 'ufl')) )
+{
+?>
+ <td width="25%" align="center">
+   <form action="index.php">
+   <input type="hidden" name="mode" value="avgProd">
+   <input type="hidden" name="tabel" value="subteamoffset">
+   <input type="Submit" value="Subteam Members" class="TextField">
+   <input type="hidden" name="prefix" value="<? echo $project->getPrefix(); ?>">
+   </form>
+ </td>
+<?
+}
+?>
+</tr>
+</table>
+<hr>
+</center>
+<br>
+<center>
+<?
+$avgProduction = new AverageList($db, $project->getPrefix() . '_' . $tabel, $datum);
+$avgProduction->gather();
+
+$avgProductionList = $avgProduction->getList();
+
+echo openColorTable(87);
+echo '<tr>';
+echo '<td></td>';
+echo '<td></td>';
+echo '<td><a href="index.php?mode=avgProd&amp;prefix=<? echo $project->getPrefix(); ?>&amp;tabel=<? echo $tabel; ?>&amp;sort=avgDaily">WeekAvg</a></td>';
+echo '<td><a href="index.php?mode=avgProd&amp;prefix=<? echo $project->getPrefix(); ?>&amp;tabel=<? echo $tabel; ?>&amp;sort=avgMonthly">MonthAvg</a></td>';
+echo '</tr>';
+
+for($member=0;$member<count($avgProductionList);$member++)
+{
+	echo trBackground($member);
+	echo '<td align="right" width="4%">' . ( $member + 1 ) . '.</td>';
+	echo '<td align="left" width="80%"><a href="index.php?mode=detail&amp;naam=' . rawurlencode($avgProductionList[$member]['realname']) . '&amp;tabel=' . $tabel . '&amp;prefix=' . $project->getPrefix() . '&amp;team=' . rawurlencode($avgProductionList[$member]['team']) . '">' . $avgProductionList[$member]['name'] . '</a></td>';
+	echo '<td align="right" width="8%" style="color:#FF0000">' . $avgProductionList[$member]['daily'] . '</td>';
+	echo '<td align="right" width="8%" style="color:#FF0000">' . $avgProductionList[$member]['monthly'] . '</td>';
+	echo '</tr>';
+}
+?>
+</table>
+</td></tr></table>
+</center>
