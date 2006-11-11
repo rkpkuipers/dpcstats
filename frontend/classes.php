f00878a18d6ab8b65d7f149c3b62adca77c472a8
rkuipers <rkuipers@26720b1a-1515-4208-a898-b861cbbdf37f>
2006-11-11 11:24:38 +0000

    Moved global variables from classes.php to classes/config.php. Added database variables. Changed opening of the db connection to use the variables. Updated database class constructors to use passed variables instead of hardcoded.
    git-svn-id: http://www.rkpk.nl/svn/dpcstats@223 26720b1a-1515-4208-a898-b861cbbdf37f

--- frontend/classes.php
+++ frontend/classes.php
@@ -1,5 +1,7 @@
 <?
 # Include the classes used 
+require ('classes/config.php');
+
 require ('classes/util.php');
 
 require ('classes/database.php');
@@ -29,35 +31,7 @@ require ('classes/AverageProduction.php');
 require ('classes/faq.php');
 
 # Initialize the database
-$db = new pgDataBase();
+$db = new miDataBase($dbuser, $dbpass, $dbhost, $dbport, $dbname);
 $db->connect();
-
-# Globals
-$listsize = 30;
-$baseUrl = 'http://tadah.mine.nu';
-
-$jpgraphdir = '/usr/share/php5/jpgraph/';
-
- $kleur[0] = "#CC0000";
- $kleur[6] = "#FFFF00";   # Rood
- $kleur[9] = "#00FF00";   # Groen
- $kleur[3] = "#0000FF";   # Blauw
- $kleur[4] = "#FFFF00";   # Geel
- $kleur[5] = "#00FFFF";   # Cyaan
- $kleur[1] = "#FF00FF";   # Paars
- $kleur[7] = "#CACACA";   # Donker Rood
- $kleur[8] = "#FF8888";   # Roze
- $kleur[2] = "#009900";   # Donker groen
-$kleur[10] = "#88FF88";   # Licht groen
-$kleur[11] = "#0000CC";   # Donker blauw
-$kleur[12] = "#8888FF";   # Blauw-grijs
-$kleur[13] = "#FF8800";   # Oranje
-$kleur[14] = "#CD5C5C";   # Grijs
-$kleur[15] = "#153F62";   # Blauw
-$kleur[16] = "#FF0000";   # Geel
-$kleur[17] = "#A52A2A";   # Brown
-
-for($i=18;$i<31;$i++)
-	$kleur[$i] = "#000000";
 
 ?>
