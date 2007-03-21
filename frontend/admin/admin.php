c91b9d3b666552cc6d84492444d1781f1d654f1b
rkuipers <rkuipers@26720b1a-1515-4208-a898-b861cbbdf37f>
2007-03-21 13:37:02 +0000

    Initial implementation of cross-project links
    git-svn-id: http://www.rkpk.nl/svn/dpcstats@285 26720b1a-1515-4208-a898-b861cbbdf37f

--- frontend/admin/admin.php
+++ frontend/admin/admin.php
@@ -1,5 +1,48 @@
 <?
 
+function crossProjectLinks()
+{
+	global $db;
+
+	$query = 'SELECT project, description FROM project ORDER BY description';
+	$result = $db->selectQuery($query);
+
+	echo '<hr>';
+	echo '<form name="cpl" action="index.php" method="post">';
+	echo '<input type="hidden" name="mode" value="admin">';
+	echo '<input type="hidden" name="action" value="cpl">';
+	echo '<center>';
+	echo '<table width="75%">';
+	while ( $line = $db->fetchArray($result) )
+	{
+		echo '<tr>';
+		echo '<td align="left">' . $line['description'] . '</td>';
+
+		echo '<td>';
+		echo '<input type="text" name="nm' . $line['project'] . '" class="TextField" value="' . $_POST['nm' . $line['project']] . '">';
+		echo '</td>';
+		if ( ( ! isset($_POST['nm' . $line['project']]) ) || ( $_POST['nm' . $line['project']] == '' ) )
+		{
+			echo '<td></td>';
+		}
+		else if ( memberExists($line['project'], 'memberoffset', $_POST['nm' . $line['project']]) )
+		{
+			# Store member cross link in database
+			# <green>Validated</green>
+			echo '<td style="color:#005500">Member exists</td>';
+		}
+		else
+		{
+			echo '<td style="color:#FF0000">Member does not exist</td>';
+		}
+		echo '</td>';
+	}
+	echo '<tr><td colspan="2"><input type="submit" value="Save" class="TextField"></td></tr>';
+	echo '</table>';
+	echo '</center>';
+	echo '</form>';
+}
+
 if ( ! isset($_SESSION['username']) )
 {
 	#header('Location: /index.php');
@@ -7,6 +50,9 @@ if ( ! isset($_SESSION['username']) )
 	return false;
 }
 echo '<center><h2>Welcome ' . $_SESSION['username'] . '</h2></center>';
+
+if ( isset($_POST['action']) )
+	$action = $_POST['action'];
 ?>
 <hr>
 <table width="95%">
@@ -14,11 +60,17 @@ echo '<center><h2>Welcome ' . $_SESSION['username'] . '</h2></center>';
   <td align="center">
    <form name="subteaminfo" action="index.php" method="post">
     <input name="mode" type="hidden" value="admin">
-    <input type="hidden" name="action" value="subteaminfo">
-    <input type="submit" value="Subteam Info" class="TextField">
+    <input type="hidden" name="action" value="cpl">
+    <input type="submit" value="Cross Project Links" class="TextField">
    </form>
   </td>
 <td align="center">SoB/F@H Members</td>
 <td align="center">Sengent Info</td>
 </tr>
 </table>
+<?
+switch ($action)
+{
+	case 'cpl':	crossProjectLinks();
+			break;
+}
