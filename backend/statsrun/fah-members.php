122d984ac1fca3c8c71fb162283d4494934d2123
rkuipers <rkuipers@26720b1a-1515-4208-a898-b861cbbdf37f>
2007-03-23 21:55:34 +0000

    Switched to using the .txt page instead of the live cgi page.
    git-svn-id: http://www.rkpk.nl/svn/dpcstats@297 26720b1a-1515-4208-a898-b861cbbdf37f

--- backend/statsrun/fah-members.php
+++ backend/statsrun/fah-members.php
@@ -12,77 +12,41 @@ dailyOffset('individualoffset', 'fah');
 $members = array();
 $subteams = array();
 
-function getMembers()
+$page = file('http://fah-web.stanford.edu/teamstats/team92.txt');
+
+$customsubteam = array(	'New-Folder0twisted' => 'New_Folder',
+			'Team_Elteor-borislavj0Missy' => 'Team_Elteor_Borislavj');
+
+for($i=0;$i<count($page);$i++)
 {
-	global $datum, $members, $subteams;
+	$data = explode("\t", $page[$i]);
+	$user = preg_replace("/<a[^>]*?>(.*)<\/a>/i", "$1", $data[2]);
+	$score = $data[3];
 
-	# Three members are placed in a subteam even though the name doesn't match
-	$customsubteam = array(	'New-Folder0twisted' => 'New_Folder', 
-				'Team_Elteor-borislavj0Missy' => 'Team_Elteor_Borislavj');
-	
-	$tempDir = '/home/rkuipers/stats/statsrun/files/';
-	system('wget "http://fah-web.stanford.edu/cgi-bin/main.py?qtype=teampage&teamnum=92" -q -O ' . $tempDir . '/fah-members.html');
-	system('cat ' . $tempDir . '/fah-members.html | grep -e fah-web.stanford.edu\/awards\/cert.php -e qtype=userpage | grep -v t=wus > ' . $tempDir . '/fah-members.txt');
-	unlink($tempDir . '/fah-members.html');
-
-	$raw = implode('', file($tempDir . '/fah-members.txt'));
-	$data = preg_replace(array('@<a[^>]*?>@si', '@</a>@si', '@<td[^>]*?>@si', '@</td>@si', '@<TD[^>]*?>@si', '@</TD>@si'), '||', $raw);
-
-	unlink($tempDir . '/fah-members.txt');
-
-	$info = explode('||', $data);
-
-	$members = array();
-	$subteams = array();
-
-	for($i=2;$i<count($info);$i+=8)
+	if ( ( is_numeric($score) ) && ( $score > 0 ) )
 	{
-		#echo 'user: ' . trim($info[$i]) . ' score: ' . trim($info[$i+4]) . "\n";
-
-		$user = trim($info[$i]);
-		$score = trim($info[$i+4]);
-
-		if ( $score == 0 ) break;
-
-		addMember($members, $subteams, (isset($customsubteam[$user])?$customsubteam[$user].'0'.$user:$user), $score, '0');
+		addMember(	$members, 
+				$subteams, 
+				(isset($customsubteam[$user])?$customsubteam[$user].'0'.$user:$user), 
+				$score, 
+				'0');
 	}
 }
-
-$itt = 0;
-do
-{
-	if ( $itt > 0 )
-	{
-		echo "No members retrieved, restarting loop\n";
-		sleep(10);
-	}
-	$itt++;
-	
-	getMembers();
-} while ( ( ( count($members) == 0 ) || ( count($subteams) == 0 ) ) && ( $itt < 25 ) );
 
 fixLists($members, $subteams, '0');
 
-updateStats($members, 'fah_memberoffset');
-
-$fahsubteammembers = array();
 foreach ( $subteams as $subTeamName => $member )
 {
-	if ( ! $subTeamName == '0' )
+	arsort($member, SORT_NUMERIC);
+	foreach ( $member as $memberName => $memberScore )
 	{
-		arsort($member, SORT_NUMERIC);
-		
-		foreach ( $member as $memberName => $memberScore )
-		{
-			$fahsubteammembers[] = new TeamMember($memberName, $memberScore, $subTeamName);
-		}
-	}
-	else
-	{
-		echo 'hit';
+		$subteammembers[] = new TeamMember($memberName, $memberScore, $subTeamName);
 	}
 }
-addSubTeamStatsRun($fahsubteammembers, 'fah_subteamoffset');
+
+updateStats($members, 'fah_memberoffset');
+
+addSubTeamStatsRun($subteammembers, 'fah_subteamoffset');
 
 individualStatsrun('fah');
 
