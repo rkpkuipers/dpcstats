2c2fd585de6f6a61f902d3f12206118786e33863
rkuipers <rkuipers@26720b1a-1515-4208-a898-b861cbbdf37f>
2007-03-25 20:11:28 +0000

    Made like statement always case insensitive by comparing upper(colum) to upper(searchstring)
    git-svn-id: http://www.rkpk.nl/svn/dpcstats@304 26720b1a-1515-4208-a898-b861cbbdf37f

--- frontend/search.php
+++ frontend/search.php
@@ -11,7 +11,7 @@ function searchTabel($tabel, $prefix, $string, $tabelNaam)
 		FROM 
 			' . $prefix . '_' . $tabel . ' 
 		WHERE 
-			naam ' . ($db->getType()=='postgres'?'I':'') . 'LIKE \'%' . $string . '%\'
+			upper(naam) LIKE \'%' . strtoupper($string) . '%\'
 		AND	dag = \'' . date("Y-m-d") . '\'';
 
         $result = $db->selectQuery($query);
