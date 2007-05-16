3ac8fb99f9adccdb75fcdebd53e121f7b911c9fe
(no author) <(no author)@26720b1a-1515-4208-a898-b861cbbdf37f>
2007-05-16 13:24:42 +0000

    Added googleadds to the bottom of the page
    git-svn-id: http://www.rkpk.nl/svn/dpcstats@316 26720b1a-1515-4208-a898-b861cbbdf37f

--- frontend/index.php
+++ frontend/index.php
@@ -386,7 +386,22 @@ if (window.attachEvent) window.attachEvent("onload", sfHover);
 	# Include the file containing the requested content
 	include($pagefile);
 ?>
-       <br>
+       <br><br><br><br><br><center>
+<script type="text/javascript"><!--
+google_ad_client = "pub-8825564864858262";
+google_ad_width = 468;
+google_ad_height = 60;
+google_ad_format = "468x60_as";
+google_ad_type = "text_image";
+google_ad_channel = "";
+google_color_border = "000000";
+google_color_bg = "AEAEAE";
+google_color_link = "000000";
+google_color_text = "333333";
+google_color_url = "005FA9";
+//--></script>
+<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+</center>
       </td>
       <td rowspan="0" style="background-image:url(images/index_r6_c11.jpg); background-position:right top; background-repeat:repeat-y" width="11" height="100%" ></td>
       <td style="background-image:url(images/spacer.gif);" width="1"></td>
