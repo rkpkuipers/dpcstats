11cf89a45bd4b5957f06d5cfd1312b049cf47c1b
(no author) <(no author)@26720b1a-1515-4208-a898-b861cbbdf37f>
2007-05-23 12:10:29 +0000

    Switch upstream source to daily_user_summary
    git-svn-id: http://www.rkpk.nl/svn/dpcstats@321 26720b1a-1515-4208-a898-b861cbbdf37f

--- backend/statsrun/fah-members.php
+++ backend/statsrun/fah-members.php
@@ -12,7 +12,7 @@ dailyOffset('individualoffset', 'fah');
 $members = array();
 $subteams = array();
 
-$page = file('http://fah-web.stanford.edu/teamstats/team92.txt');
+$page = file('http://fah-web.stanford.edu/daily_user_summary.txt');
 
 $customsubteam = array(	'New-Folder0twisted' => 'New_Folder',
 			'Team_Elteor-borislavj0Missy' => 'Team_Elteor_Borislavj');
@@ -20,16 +20,20 @@ $customsubteam = array(	'New-Folder0twisted' => 'New_Folder',
 for($i=0;$i<count($page);$i++)
 {
 	$data = explode("\t", $page[$i]);
-	$user = preg_replace("/<a[^>]*?>(.*)<\/a>/i", "$1", $data[2]);
-	$score = $data[3];
 
-	if ( ( is_numeric($score) ) && ( $score > 0 ) )
+	if ($data[3] == 92 )
 	{
-		addMember(	$members, 
-				$subteams, 
-				(isset($customsubteam[$user])?$customsubteam[$user].'0'.$user:$user), 
-				$score, 
-				'0');
+		$user = preg_replace("/<a[^>]*?>(.*)<\/a>/i", "$1", $data[0]);
+		$score = $data[1];
+
+		if ( ( is_numeric($score) ) && ( $score > 0 ) )
+		{
+			addMember(	$members, 
+					$subteams, 
+					(isset($customsubteam[$user])?$customsubteam[$user].'0'.$user:$user), 
+					$score, 
+					'0');
+		}
 	}
 }
 
