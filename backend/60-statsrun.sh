1e2ab4f3654a4cbc101fc985cd9ffc38e72912fe
(no author) <(no author)@26720b1a-1515-4208-a898-b861cbbdf37f>
2007-05-23 12:08:18 +0000

    Cleaned file. Replaced killall with more specific pkill.
    git-svn-id: http://www.rkpk.nl/svn/dpcstats@318 26720b1a-1515-4208-a898-b861cbbdf37f

--- backend/60-statsrun.sh
+++ backend/60-statsrun.sh
@@ -2,15 +2,5 @@
 
 /home/rkuipers/stats/statsrun/sob.php
 
-#/home/rkuipers/stats/statsrun/rah.php
-
-#/home/rkuipers/stats/statsrun/smp.php
-
-#/home/rkuipers/stats/statsrun/ufl.php
-
-#/home/rkuipers/stats/statsrun/eah.php
-
-#/home/rkuipers/stats/statsrun/ldc.php
-
-killall -q statsrun.php
+pkill -f "statsrun.php 60"
 /home/rkuipers/stats/statsrun/statsrun.php 60
