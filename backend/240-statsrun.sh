49c5c298cf0712bfa06d7c77720c57507969c48a
(no author) <(no author)@26720b1a-1515-4208-a898-b861cbbdf37f>
2007-05-23 12:14:23 +0000

    Removed UD from the statsrun since the project called it quits some time ago
    git-svn-id: http://www.rkpk.nl/svn/dpcstats@324 26720b1a-1515-4208-a898-b861cbbdf37f

--- backend/240-statsrun.sh
+++ backend/240-statsrun.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-killall -q sah.php ud-teams.php ud-members.php
+killall -q sah.php 
 
 /home/rkuipers/stats/statsrun/wcg-members.php
 /home/rkuipers/stats/statsrun/wcg-teams.php
-
-/home/rkuipers/stats/statsrun/ud-teams.php
-/home/rkuipers/stats/statsrun/ud-members.php
 
 /home/rkuipers/stats/statsrun/sah.php
