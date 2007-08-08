9ea44d7c369f22a52c939f96ef0b75aab31a3689
rkuipers <rkuipers@26720b1a-1515-4208-a898-b861cbbdf37f>
2007-08-08 19:36:57 +0000

    Update each table seperately instead of all tables in a single statement Add a 15 second sleep between tables to prevent locks
    git-svn-id: http://www.rkpk.nl/svn/dpcstats@331 26720b1a-1515-4208-a898-b861cbbdf37f

--- backend/scripts/optimize.php
+++ backend/scripts/optimize.php
@@ -8,19 +8,22 @@ $db->connect();
 
 $result = $db->selectQuery('SHOW TABLES FROM stats');
 
-$optQuery = 'OPTIMIZE TABLE ';
-
-$i = 0;
+$tables = array();
 while ( $line = $db->fetchArray($result) )
 {
-	if ( $i > 0 ) $optQuery .= ',';
-	else $i++;
-	$optQuery .= '`' . $line['0'] . '`';
-#	echo $line['0'];
+	$tables[] = $line['0'];
 }
-#echo $optQuery;
-$result = $db->selectQuery($optQuery);
 
-while ( $line = $db->fetchArray($result))
-	echo $line['0'] . "\r\t\t\t" . $line['1'] . "\t" . $line['2'] . "\t" . $line['3'] . "\n";
+foreach($tables as $table)
+{
+	$query = 'OPTIMIZE TABLE ' . $table;
+	$result = $db->selectQuery($query);
+
+	while ( $line = $db->fetchArray($result))
+		echo $line['0'] . "\r\t\t\t\t" . $line['1'] . "\t" . $line['2'] . "\t" . $line['3'];
+	
+	sleep(15);
+
+	echo "\n";
+}
 ?>
