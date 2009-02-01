517e2e03c3a8d0ed9e2b43c63cb2244e8c4d27c6
rkuipers <rkuipers@26720b1a-1515-4208-a898-b861cbbdf37f>
2009-02-01 19:58:24 +0000

    Add a header line Remove the dependency on the shoutbox and message classes Inline everything we need
    git-svn-id: http://www.rkpk.nl/svn/dpcstats@361 26720b1a-1515-4208-a898-b861cbbdf37f

--- frontend/shoutbox.php
+++ frontend/shoutbox.php
@@ -1,20 +1,80 @@
 <?
-$shoutbox = New ShoutBox($db);
-$shoutbox->getMessages();
-$messages = $shoutbox->getMessageList();
 
-echo '<center>';
-echo '<table width="98%">';
-
-for($i=0;$i<count($messages);$i++)
+function parseSmiley($bericht)
 {
+	$text = $bericht;
+	
+	$text = str_replace(':-)',  '<img src="images/smilies/smile.gif" alt=":)">',   $text);
+	$text = str_replace(':)',   '<img src="images/smilies/smile.gif" alt=":)">',   $text);
+	
+	$text = str_replace(':-D',  '<img src="images/smilies/biggrin.gif" alt=":D">', $text);
+	$text = str_replace(':D',   '<img src="images/smilies/biggrin.gif" alt=":D">', $text);
+	
+	$text = str_replace(':-+',  '<img src="images/smilies/clown.gif" alt=":+">',   $text);
+	$text = str_replace(':+',   '<img src="images/smilies/clown.gif" alt=":+">',   $text);
+	
+	$text = str_replace(':\'(', '<img src="images/smilies/cry.gif" alt=":\'(">',     $text);
+	
+	$text = str_replace('>:)',  '<img src="images/smilies/devil.gif" alt=">:)">',   $text);
+	
+	$text = str_replace(' :(',   '<img src="images/smilies/frown.gif" alt=":(">',   $text);
+	$text = str_replace(' :-(',  '<img src="images/smilies/frown.gif" alt=":(">',   $text);
+	
+	$text = str_replace('|:(',  '<img src="images/smilies/frusty.gif" alt="|:(">',  $text);
+	
+	$text = str_replace(':9~',  '<img src="images/smilies/kwijl.gif" alt=":9~">',   $text);
+	
+	$text = str_replace(':p',   '<img src="images/smilies/puh2.gif" alt=":p">',    $text);
+	$text = str_replace(':P',   '<img src="images/smilies/puh2.gif" alt=":p">',    $text);
+	
+	$text = str_replace(':r',   '<img src="images/smilies/pukey.gif" alt=":r">',   $text);
+	
+	$text = str_replace(':o',   '<img src="images/smilies/redface.gif" alt=":o">', $text);
+	$text = str_replace(':O',   '<img src="images/smilies/redface.gif" alt=":o">', $text);
+	
+	$text = str_replace(';)',   '<img src="images/smilies/wink.gif" alt=";)">',    $text);
+	$text = str_replace(';-)',  '<img src="images/smilies/wink.gif" alt=";)">',    $text);
+	
+	$text = str_replace(':w',   '<img src="images/smilies/bye.gif" alt=":w">',      $text);
+	
+	return $text;
+}
+
+echo '<div style="margin-left:auto; margin-right:auto; text-align:center;"><h2>Shoutbox</h2></div>';
+
+# Use a centered table
+echo '<table style="width:98%; margin-left:auto; margin-right:auto;">';
+
+# Retrieve the shoutbox elements from the database
+$query = 'SELECT 
+		naam,
+		bericht,
+		geplaatst,
+		email
+	FROM
+		shoutbox
+	ORDER BY
+		geplaatst DESC';
+
+# Execute the query
+$result = $db->selectQuery($query);
+
+# Loop through the results
+while ( $line = $db->fetchArray($result) )
+{
+	# Start the table row and first cell
 	echo trBackground(0);
 	echo '<td>';
-	if ( $messages[$i]->getEmail() != '' )
-		echo '<a href="mailto:' . $messages[$i]->getEmail() . '">' .  $messages[$i]->getPoster() . '</a>';
+	
+	# If an email adres was provided, link the postername
+	if ( ! empty($line['email']) )
+		echo '<a href="mailto:' . $line['email'] . '">' .  $line['naam'] . '</a>';
 	else
-		echo $messages[$i]->getPoster();
-	echo ' - ' . $messages[$i]->getTijd() . '</td>';
+		echo $line['naam'];
+	
+	# Show the time the message was places next to the name of the poster
+	echo ' - ' . $line['geplaatst'] . '</td>';
+
 	echo '</tr>';
 	echo trBackground( ( $i + 1 ) );
 	echo '<td>';
@@ -22,11 +82,11 @@ for($i=0;$i<count($messages);$i++)
 	# Replace complete URL's with an actual link
 	echo preg_replace(	array('/http\:\/\/(.\*)(\b|$|\s|\n||\)|\z|\Z)/'),
 				array('<a href="\0">link</a>'),
-				$messages[$i]->getBericht());
+				$line['bericht']);
 	echo '</td>';
 	echo '</tr>';
 	echo '<tr><td><br></td></tr>';
 }
 echo '</table>';
-echo '</center>';
+
 ?>
