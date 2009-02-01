8715d6c20b72830d8dad6f353c6d910f59e19bdf
rkuipers <rkuipers@26720b1a-1515-4208-a898-b861cbbdf37f>
2009-02-01 20:14:00 +0000

    Removed unused function
    git-svn-id: http://www.rkpk.nl/svn/dpcstats@365 26720b1a-1515-4208-a898-b861cbbdf37f

--- frontend/shoutbox.php
+++ frontend/shoutbox.php
@@ -1,45 +1,5 @@
 <?
 
-function parseSmiley($bericht)
-{
-	$text = $bericht;
-	
-	$text = str_replace(':-)',  '<img src="images/smilies/smile.gif" alt=":)">',   $text);
-	$text = str_replace(':)',   '<img src="images/smilies/smile.gif" alt=":)">',   $text);
-	
-	$text = str_replace(':-D',  '<img src="images/smilies/biggrin.gif" alt=":D">', $text);
-	$text = str_replace(':D',   '<img src="images/smilies/biggrin.gif" alt=":D">', $text);
-	
-	$text = str_replace(':-+',  '<img src="images/smilies/clown.gif" alt=":+">',   $text);
-	$text = str_replace(':+',   '<img src="images/smilies/clown.gif" alt=":+">',   $text);
-	
-	$text = str_replace(':\'(', '<img src="images/smilies/cry.gif" alt=":\'(">',     $text);
-	
-	$text = str_replace('>:)',  '<img src="images/smilies/devil.gif" alt=">:)">',   $text);
-	
-	$text = str_replace(' :(',   '<img src="images/smilies/frown.gif" alt=":(">',   $text);
-	$text = str_replace(' :-(',  '<img src="images/smilies/frown.gif" alt=":(">',   $text);
-	
-	$text = str_replace('|:(',  '<img src="images/smilies/frusty.gif" alt="|:(">',  $text);
-	
-	$text = str_replace(':9~',  '<img src="images/smilies/kwijl.gif" alt=":9~">',   $text);
-	
-	$text = str_replace(':p',   '<img src="images/smilies/puh2.gif" alt=":p">',    $text);
-	$text = str_replace(':P',   '<img src="images/smilies/puh2.gif" alt=":p">',    $text);
-	
-	$text = str_replace(':r',   '<img src="images/smilies/pukey.gif" alt=":r">',   $text);
-	
-	$text = str_replace(':o',   '<img src="images/smilies/redface.gif" alt=":o">', $text);
-	$text = str_replace(':O',   '<img src="images/smilies/redface.gif" alt=":o">', $text);
-	
-	$text = str_replace(';)',   '<img src="images/smilies/wink.gif" alt=";)">',    $text);
-	$text = str_replace(';-)',  '<img src="images/smilies/wink.gif" alt=";)">',    $text);
-	
-	$text = str_replace(':w',   '<img src="images/smilies/bye.gif" alt=":w">',      $text);
-	
-	return $text;
-}
-
 echo '<div style="margin-left:auto; margin-right:auto; text-align:center;"><h2>Shoutbox</h2></div>';
 
 # Use a centered table
