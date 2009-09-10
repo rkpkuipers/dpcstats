57c5fda521716f726e03f41197b9404da536efc7
rkuipers <rkuipers@26720b1a-1515-4208-a898-b861cbbdf37f>
2009-09-10 12:48:01 +0000

    Script to insert the top5 flushers list back into the db after a cleanup Example script of the data
    git-svn-id: http://www.rkpk.nl/svn/dpcstats@379 26720b1a-1515-4208-a898-b861cbbdf37f

---
+++ backend/scripts/insertTop.php
@@ -0,0 +1,22 @@
+#!/usr/bin/php
+<?
+
+include('/home/rkuipers/public_html/classes.php');
+
+$table = $argv[1];
+
+$data = file('data.txt');
+
+for($line=0;$line<count($data);$line++)
+{
+	$fields = explode("\t", $data[$line]);
+	$query = 'INSERT INTO ' . 
+			$table . 
+		' ( naam, dag, daily ) VALUES (\'' . $fields[1] . '\', ' .
+		'\'' . preg_replace('/([0-9]{2})-([0-9]{2})-([0-9]{4})/', '\3-\2-\1', trim($fields[3])) . '\', ' . str_replace('.', '', $fields[2]) . ')';
+	echo $query . "\n";
+	$db->insertQuery($query);
+}
+
+$db->disconnect();
+?>
