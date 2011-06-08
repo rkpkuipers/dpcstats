628f17b5fc1f62dbe7e673c43a50ab5e772b2277
rkuipers <rkuipers@26720b1a-1515-4208-a898-b861cbbdf37f>
2011-06-08 10:21:38 +0000

    Update copyright year, drop the traffic link and fix the version link
    git-svn-id: http://www.rkpk.nl/svn/dpcstats@469 26720b1a-1515-4208-a898-b861cbbdf37f

--- frontend/index.php
+++ frontend/index.php
@@ -249,9 +249,8 @@ header('Content-Type: text/html; charset=ISO-8859-1');
      <tr>
       <td colspan="12" class="pageCell" style="background-image:url(images/index_r12_c1.jpg); background-position:right top; background-repeat:repeat-y" align="center">
        <a href="mailto:speedkikker@planet.nl" title="Mail SpeedKikker">Contact</a> 
-       | <a href="http://tadah.mine.nu/traffic/awstats.tadah.mine.nu.html" title="View site traffic">Site Traffic</a>
-       | &copy;opyright 2004-2008 TaDaH
-       | <a href="http://rkuipers.mine.nu/viewcvs/" title="View revision history">Revision <?php echo $project->getVersion();?></a> 
+       | &copy;opyright 2004-<?php echo date("Y"); ?> TaDaH
+       | <a href="http://rkuipers.mine.nu/svn/frontend/" title="View revision history">Revision <?php echo $project->getVersion();?></a> 
        | <?php echo 'Page loaded in ' . sprintf("%0.3f", microtime_diff($start_time, microtime())) . ' seconds'; ?>
       </td>
       <td><img src="images/spacer.gif" width="1" height="34" alt=""></td>
