c47d12117fab8fc692d5a9d46cc793f4431206d6
rkuipers <rkuipers@26720b1a-1515-4208-a898-b861cbbdf37f>
2011-07-01 08:30:02 +0000

    Repositories were merged
    git-svn-id: http://www.rkpk.nl/svn/dpcstats@496 26720b1a-1515-4208-a898-b861cbbdf37f

--- backend/scripts/export.sh
+++ backend/scripts/export.sh
@@ -6,25 +6,16 @@ EXPORTDIR=/var/www/tadah.mine.nu/export/
 'rm' -rf "${EXPORTDIR}/frontend/"
 
 # Export the svn source
-svn export http://rkuipers.mine.nu/svn/frontend ${EXPORTDIR}/frontend
+svn export http://rkuipers.mine.nu/svn/dpcstats ${EXPORTDIR}/dpcstats/
 
 # Create the tar file
-cd ${EXPORTDIR} && tar -cf frontend.tar frontend/
+cd ${EXPORTDIR} && tar -cf stats.tar dpcstats/
 
 # Zip the file
-bzip2 -f ${EXPORTDIR}/frontend.tar
+bzip2 -f ${EXPORTDIR}/stats.tar
 
 # Drop the backend directory
-'rm' -rf "${EXPORTDIR}/backend/"
-
-# Export the svn source
-svn export http://rkuipers.mine.nu/svn/backend ${EXPORTDIR}/backend
-
-# Create the tar file
-cd ${EXPORTDIR} && tar -cf backend.tar backend/
-
-# Zip the file
-bzip2 -f ${EXPORTDIR}/backend.tar
+'rm' -rf "${EXPORTDIR}/dpcstats/"
 
 # Dump the database scheme
 mysqldump -h 192.168.0.7 stats -d > ${EXPORTDIR}/database.sql
